168b23037ce0d903709fd72dd2168149c4b3adba
andres781 <marruandres2003@gmail.com>
2024-08-13 09:26:41 -0500

    Create Codigo1.py

---
+++ Corte1/Codigo1.py
@@ -0,0 +1,7 @@
+print("Mi primera vez en python")
+a = "Cadena de caracteres"
+print("Tipo de dato a: ",type(a))
+b = 8
+print("Tipo de dato b: ", type(b))
+c = 3.1416
+print("Tipo de dato c: ",type(c))
